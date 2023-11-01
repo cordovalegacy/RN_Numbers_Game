522021b786558743e391f35697d7f42a546c3a34
Brendan Cordova <cordovalegacy19@gmail.com>
2023-11-01 15:32:27 -0700

    pseudo coding out next steps

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -5,6 +5,10 @@ import { Pressable, Text, View } from "react-native"
 // !Styles
 import { gameStyles } from "../styles/gameStyles"
 
+//next we want to write a function that generates a random number
+//then we will want to compare the input from the user with that random number
+//this will probably be inside a while loop because we need the value to persist
+
 export default function GameScreen() {
     return (
         <View style={gameStyles.gameContainer}>
